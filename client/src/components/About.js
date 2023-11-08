c0c34a6387c1d09233428569331a01c8a19699a8
TheresaHHH <hsuanhuahung@ufl.edu>
2023-11-07 20:53:11 -0500

    changed logos

---
+++ client/src/components/About.js
@@ -0,0 +1,7 @@
+// src/About.jsimport React from 'react';
+
+function About() {
+  return <div>About Page</div>;
+}
+
+export default About;
---
+++ client/src/components/FAQs.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FAQs() {
+  return <div>FAQs Page</div>;
+}
+
+export default FAQs;
